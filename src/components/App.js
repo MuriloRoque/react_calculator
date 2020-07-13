dc9805b2c072419cdcf68867ddbd83329f46913e
Murilo Roque <muriloengqui@gmail.com>
2020-07-13 03:54:46 -0300

    improve project's structural organization

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,10 @@
 import React from 'react';
-import './App.css';
+import '../styles/App.css';
 
 function App() {
   return (
     <div>
-      <h1>Welcome to the Calculator!</h1>
+      <h1 className='title'>Welcome to the Calculator!</h1>
     </div>
   );
 }
--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { render } from '@testing-library/react';
-import App from './App';
+import App from '../components/App';
 
 test('renders welcome to calculator', () => {
   const { getByText } = render(<App />);
