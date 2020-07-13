e8ed0f12dc27a3abe3d4784a3613d1de1c99afcb
Murilo Roque <muriloengqui@gmail.com>
2020-07-13 16:32:51 -0300

    fix tests

--- src/tests/Button.test.js
+++ src/tests/Button.test.js
@@ -2,14 +2,16 @@ import React from 'react';
 import { render } from '@testing-library/react';
 import Button from '../components/Button';
 
+const handleClick = () => { ''; };
+
 test('renders the button texts correctly', () => {
-  const { getByText } = render(<Button name="=" wide={false} />);
+  const { getByText } = render(<Button name="=" wide={false} handleClick={handleClick} />);
   const equalButton = getByText(/=/i);
   expect(equalButton).toBeInTheDocument();
 });
 
 test('the button roles are button', () => {
-  const { getByRole } = render(<Button name="=" wide={false} />);
+  const { getByRole } = render(<Button name="=" wide={false} handleClick={handleClick} />);
   const equalButton = getByRole('button');
   expect(equalButton).toBeInTheDocument();
 });
