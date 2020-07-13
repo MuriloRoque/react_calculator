711486f4914c0984f18f870eee5eb1f423fa2e7b
Murilo Roque <muriloengqui@gmail.com>
2020-07-13 04:41:27 -0300

    implement ButtonPanel Buttons

--- src/components/ButtonPanel.js
+++ src/components/ButtonPanel.js
@@ -10,15 +10,35 @@ class ButtonPanel extends React.Component {
   render() {
     return(
       <div id='button-panel'>
-        <Button name='1' />
-        <Button name='2' />
-        <Button name='3' />
-        <Button name='4' />
-        <Button name='5' />
-        <Button name='6' />
-        <Button name='7' />
-        <Button name='8' />
-        <Button name='9' />
+        <div className='row'>
+          <Button name='AC' />
+          <Button name='+/-' />
+          <Button name='%' />
+          <Button name='÷' />
+        </div>
+        <div className='row'>
+          <Button name='7' />
+          <Button name='8' />
+          <Button name='9' />
+          <Button name='x' />
+        </div>
+        <div className='row'>
+          <Button name='4' />
+          <Button name='5' />
+          <Button name='6' />
+          <Button name='-' />
+        </div>
+        <div className='row'>
+          <Button name='1' />
+          <Button name='2' />
+          <Button name='3' />
+          <Button name='+' />
+        </div>
+        <div className='row'>
+          <Button name='0' />
+          <Button name='.' />
+          <Button name='=' />
+        </div>
       </div>
     )};
 }
