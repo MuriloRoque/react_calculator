eaba4332e440b1925c056f2976eef17545f30835
Murilo Roque <muriloengqui@gmail.com>
2020-07-13 15:50:46 -0300

    implement App stateful component with handleclick function

--- src/components/App.js
+++ src/components/App.js
@@ -2,12 +2,30 @@ import React from 'react';
 import Display from './Display';
 import ButtonPanel from './ButtonPanel';
 import '../styles/css/App.css';
+import React, { Component } from 'react';
+import calculate from '../logic/calculate';
 
-const App = () => (
-  <div id="parent-div">
-    <Display />
-    <ButtonPanel />
-  </div>
-);
+class App extends Component{
+  constructor(props){
+    super(props);
+    this.state = {
+      total: null,
+      next: null,
+      operation: null,
+    }
+    this.handleClick = this.handleClick.bind(this);
+  }
+  handleClick(buttonName) {
+    const { total, next, operation } = this.state;
+    const calculator = calculate({ total, next, operation }, buttonName);
+    this.setState(calculator);
+  }
+  render(){
+    <div id="parent-div">
+      <Display result={this.result} />
+      <ButtonPanel handleClick={this.handleClick}/>
+    </div>
+  }
+};
 
 export default App;
