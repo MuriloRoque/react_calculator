a25cfe33f484a09fe4cbd8a1be8194f72f2a8a54
Murilo Roque <muriloengqui@gmail.com>
2020-07-13 13:58:49 -0300

    fix linter errors

--- src/tests/calculate.test.js
+++ src/tests/calculate.test.js
@@ -1,48 +1,48 @@
-import calculations from '../logic/calculate'
-import { expect } from 'chai'
+import { expect } from 'chai';
 import Big from 'big.js';
+import calculations from '../logic/calculate';
 
 test('AC button', () => {
-  const calculator = { total: '1', next: '5', operation: '+' }
-  expect(calculations.calculate(calculator, 'AC')).to.be.eql({ total: null, next: null, operation: null })
-})
+  const calculator = { total: '1', next: '5', operation: '+' };
+  expect(calculations.calculate(calculator, 'AC')).to.be.eql({ total: null, next: null, operation: null });
+});
 
 test('= button', () => {
-  const calculator = { total: '1', next: '5', operation: '+' }
-  expect(calculations.calculate(calculator, '=')).to.eql({ total: Big(6), next: null, operation: null })
-})
+  const calculator = { total: '1', next: '5', operation: '+' };
+  expect(calculations.calculate(calculator, '=')).to.eql({ total: Big(6), next: null, operation: null });
+});
 
 test('operation with full calculator object', () => {
-  const calculator = { total: '1', next: '5', operation: '+' }
-  expect(calculations.calculate(calculator, '+')).to.eql({ total: Big(6), next: null, operation: '+' })
-})
+  const calculator = { total: '1', next: '5', operation: '+' };
+  expect(calculations.calculate(calculator, '+')).to.eql({ total: Big(6), next: null, operation: '+' });
+});
 
 test('next without operation', () => {
-  const calculator = { total: '1', next: '5', operation: null }
-  expect(calculations.calculate(calculator, '+')).to.eql({ total: '5', next: null, operation: '+' })
-})
+  const calculator = { total: '1', next: '5', operation: null };
+  expect(calculations.calculate(calculator, '+')).to.eql({ total: '5', next: null, operation: '+' });
+});
 
 test('only operation button', () => {
-  const calculator = { total: '1', next: null, operation: null }
-  expect(calculations.calculate(calculator, '+')).to.eql({ total: '1', next: null, operation: '+' })
-})
+  const calculator = { total: '1', next: null, operation: null };
+  expect(calculations.calculate(calculator, '+')).to.eql({ total: '1', next: null, operation: '+' });
+});
 
 test('+/- button with next without total', () => {
-  const calculator = { total: null, next: '5', operation: null }
-  expect(calculations.calculate(calculator, '+/-')).to.eql({ total: null, next: Big('-5'), operation: null })
-})
+  const calculator = { total: null, next: '5', operation: null };
+  expect(calculations.calculate(calculator, '+/-')).to.eql({ total: null, next: Big('-5'), operation: null });
+});
 
 test('+/- button with total', () => {
-  const calculator = { total: '5', next: null, operation: null }
-  expect(calculations.calculate(calculator, '+/-')).to.eql({ total: Big('-5'), next: null, operation: null })
-})
+  const calculator = { total: '5', next: null, operation: null };
+  expect(calculations.calculate(calculator, '+/-')).to.eql({ total: Big('-5'), next: null, operation: null });
+});
 
 test('only next', () => {
-  const calculator = { total: null, next: '5', operation: null }
-  expect(calculations.calculate(calculator, '5')).to.eql({ total: null, next: '55', operation: null })
-})
+  const calculator = { total: null, next: '5', operation: null };
+  expect(calculations.calculate(calculator, '5')).to.eql({ total: null, next: '55', operation: null });
+});
 
 test('without next', () => {
-  const calculator = { total: null, next: null, operation: null }
-  expect(calculations.calculate(calculator, '5')).to.eql({ total: null, next: '5', operation: null })
-})
+  const calculator = { total: null, next: null, operation: null };
+  expect(calculations.calculate(calculator, '5')).to.eql({ total: null, next: '5', operation: null });
+});
--- src/tests/operate.test.js
+++ src/tests/operate.test.js
@@ -1,26 +1,26 @@
-import operations from '../logic/operate'
-import { expect } from 'chai'
+import { expect } from 'chai';
 import Big from 'big.js';
+import operations from '../logic/operate';
 
 const numberOne = 2;
 const numberTwo = 10;
 
 test('the sum of two numbers', () => {
-  expect(operations.operate(numberOne, numberTwo, '+')).to.be.eql(Big(12))
-})
+  expect(operations.operate(numberOne, numberTwo, '+')).to.be.eql(Big(12));
+});
 
 test('the difference of two numbers', () => {
-  expect(operations.operate(numberOne, numberTwo, '-')).to.be.eql(Big(-8))
-})
+  expect(operations.operate(numberOne, numberTwo, '-')).to.be.eql(Big(-8));
+});
 
 test('the division of two numbers', () => {
-  expect(operations.operate(numberOne, numberTwo, '÷')).to.be.eql(Big(0.2))
-})
+  expect(operations.operate(numberOne, numberTwo, '÷')).to.be.eql(Big(0.2));
+});
 
 test('the multiplication of two numbers', () => {
-  expect(operations.operate(numberOne, numberTwo, 'x')).to.be.eql(Big(20))
-})
+  expect(operations.operate(numberOne, numberTwo, 'x')).to.be.eql(Big(20));
+});
 
 test('the percentage of two numbers', () => {
-  expect(operations.operate(numberOne, numberTwo, '%')).to.be.eql(Big(0.2))
-})
+  expect(operations.operate(numberOne, numberTwo, '%')).to.be.eql(Big(0.2));
+});
